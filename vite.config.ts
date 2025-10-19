7eaf77dbcfded970d4e6a2c524e6e0805a2f0f1e
pranavgundu <pranav@gundu.me>
2025-10-18 20:41:46 -0400

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,65 +1,66 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        VitePWA({ 
-            injectRegister: "auto",
-            registerType: "autoUpdate",
-            workbox: {
-                globPatterns: ["**/*.{js,html,png}"],
-                cleanupOutdatedCaches: true,
-                runtimeCaching: [
-                    {
-                        urlPattern: ({ request }) => request.mode === "navigate",
-                        handler: "NetworkFirst",
-                        options: {
-                            cacheableResponse: { statuses: [0, 200] }
-                        }
-                    },
-                    {
-                        urlPattern: /\.(?:js|html)$/,
-                        handler: "StaleWhileRevalidate",
-                    },
-                    {
-                        urlPattern: /\.(?:png)$/,
-                        handler: "CacheFirst",
-                    },
-                    {
-                        urlPattern: /index.html/,
-                        handler: "CacheFirst",
-                    },
-                ]
+  plugins: [
+    tailwindcss(),
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,html,png}"],
+        cleanupOutdatedCaches: true,
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.mode === "navigate",
+            handler: "NetworkFirst",
+            options: {
+              cacheableResponse: { statuses: [0, 200] },
             },
-            manifest: {
-                name: "StrategyBoard2025",
-                short_name: "Strategy2025",
-                description: "FRC2025 Strategy Board",
-                theme_color: "#ffffff",
-                icons: [
-                    {
-                        src: "favicon/andriod-chrome-192x192.png",
-                        sizes: "192x192",
-                        type: "image/png"
-                    },
-                    {
-                        src: "favicon/andriod-chrome-512x512.png",
-                        sizes: "512x512",
-                        type: "image/png"
-                    }
-                ]
-            }
-        })
-    ],
-    build: {
-        target: "es2022"
+          },
+          {
+            urlPattern: /\.(?:js|html)$/,
+            handler: "StaleWhileRevalidate",
+          },
+          {
+            urlPattern: /\.(?:png)$/,
+            handler: "CacheFirst",
+          },
+          {
+            urlPattern: /index.html/,
+            handler: "CacheFirst",
+          },
+        ],
+      },
+      manifest: {
+        name: "StrategyBoard2025",
+        short_name: "Strategy2025",
+        description: "FRC2025 Strategy Board",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "favicon/andriod-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "favicon/andriod-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    target: "es2022",
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    resolve: {
-        alias: {
-            "@": new URL("./src", import.meta.url).pathname
-        }
-    },
-    base: "/StrategyBoard2025"
+  },
+  base: "/StrategyBoard2025",
 });
